c8b1ca7d95328a0adf730fa620ca996b4319fd34
ClaySamuelC <claysamuelc@gmail.com>
2021-04-05 05:04:03 -0500

    Add faceUV to die importing an image, todo: make import local asset instead

--- src/index.js
+++ src/index.js
@@ -13,9 +13,23 @@ const createScene = () => {
 };
 
 const createDie = () => {
-  const die = BABYLON.MeshBuilder.CreateBox("die", {});
+  const dieMat = new BABYLON.StandardMaterial("dieMat");
+  dieMat.diffuseTexture = new BABYLON.Texture("https://assets.babylonjs.com/environments/cubehouse.png"); // TODO: make import assets/dieFace.jpg
+
+  const faceUV = [];
+  faceUV[0] = new BABYLON.Vector4(4/6, 0.0, 5/6, 1.0); // back: 5
+  faceUV[1] = new BABYLON.Vector4(1/6, 0.0, 2/6, 1.0); // front: 2
+  faceUV[2] = new BABYLON.Vector4(2/6, 0.0, 3/6, 1.0); // right: 3
+  faceUV[3] = new BABYLON.Vector4(3/6, 0.0, 4/6, 1.0); // left: 4
+  faceUV[4] = new BABYLON.Vector4(0.0, 0.0, 1/6, 1.0); // top: 1
+  faceUV[5] = new BABYLON.Vector4(5/6, 0.0, 6/6, 1.0); // bottom: 6
+
+  const die = BABYLON.MeshBuilder.CreateBox("die", {faceUV: faceUV, wrap: true});
+  die.material = dieMat;
 
   die.position.y = 0.5;
+  die.rotation.z = Math.PI / 4;
+  die.position.y += 0.2;
 
   return die;
 };
